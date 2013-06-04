f49bf882873983c065b75014a9770c4b3cfc3642
Dylan Kirkby <dylan.kirkby@gmail.com>
2013-06-04 14:32:17 +0200

    Add standard second values for DIT and DAH

--- morseDecoder.py
+++ morseDecoder.py
@@ -19,7 +19,7 @@ DAH=2
 #globals
 pressedDownTime = 0
 pressedUpTime = 0
-pressedDurations = [ 0.3]
+pressedDurations = [ 0.3, 0.6 ]
 
 def buttonPressed(channel):
     """Callback called when GPIO detects button is pressed"""
