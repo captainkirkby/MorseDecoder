0b320bb4c76de6970526535986ded66d86ee573d
Dylan Kirkby <dylan.kirkby@gmail.com>
2013-06-04 10:43:14 +0200

    Started to implement list of recent durations[C

--- morseDecoder.py
+++ morseDecoder.py
@@ -11,6 +11,7 @@ STANDARD_BOUNCE_TIME = 33
 THRESHOLD=0.8
 MIN_KEY_TIME=0.20
 MIN_DELAY_TIME=0.10
+LEARNING_CYCLE = 8
 
 #globals
 pressedDownTime = 0
@@ -18,49 +19,57 @@ pressedUpTime = 0
 pressedDurations = [ ]
 
 def buttonPressed(channel):
-	global pressedUpTime
-	pressedDuration = time.time() - pressedUpTime
-	if pressedDuration <= MIN_DELAY_TIME:
-		return
-	else:
-		print("Button Depressed")
-		global pressedDownTime
-		pressedDownTime=time.time()
-		GPIO.remove_event_detect(23)
-		GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-		GPIO.add_event_detect(23, GPIO.RISING, callback=buttonReleased)
+    global pressedUpTime
+    pressedDuration = time.time() - pressedUpTime
+    if pressedDuration <= MIN_DELAY_TIME:
+        return
+    else:
+        print("Button Depressed")
+        global pressedDownTime
+        pressedDownTime=time.time()
+        GPIO.remove_event_detect(23)
+        GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(23, GPIO.RISING, callback=buttonReleased)
 
 def buttonReleased(channel):
-	global pressedDownTime
-	pressedDuration = time.time() - pressedDownTime
-	if pressedDuration <= MIN_KEY_TIME:
-		return
-	else:
-		global pressedUpTime
-		pressedUpTime = time.time()
-		print("Button Released")
-		GPIO.remove_event_detect(23)
-		GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-		GPIO.add_event_detect(23, GPIO.FALLING, callback=buttonPressed)
-		print(pressedDuration)
+    global pressedDownTime
+    pressedDuration = time.time() - pressedDownTime
+    if pressedDuration <= MIN_KEY_TIME:
+        return
+    else:
+        global pressedUpTime
+        pressedUpTime = time.time()
+        print("Button Released")
+        GPIO.remove_event_detect(23)
+        GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(23, GPIO.FALLING, callback=buttonPressed)
+        
+        #record the last few durations
+        global pressedDurations
+        if len(pressedDurations) >= LEARNING_CYCLE:
+            #remove last item
+            del pressedDurations[(len(pressedDurations)-1)]
+        
+        pressedDurations.append(pressedDuration)
+        print(pressedDuration)
 
 def minimumBounceTime(maxRpm):
-	result = 0
-	try:
-		result = (1.0/float(maxRpm/60.0))*1000*THRESHOLD
-	except ZeroDivisionError:
-		result = STANDARD_BOUNCE_TIME
-	return int(result)
+    result = 0
+    try:
+        result = (1.0/float(maxRpm/60.0))*1000*THRESHOLD
+    except ZeroDivisionError:
+        result = STANDARD_BOUNCE_TIME
+    return int(result)
 
 GPIO.add_event_detect(23, GPIO.FALLING,
-		callback=buttonPressed,
-		bouncetime=minimumBounceTime(900))
+        callback=buttonPressed,
+        bouncetime=minimumBounceTime(900))
 
 try:
-	GPIO.wait_for_edge(24, GPIO.FALLING)
+    GPIO.wait_for_edge(24, GPIO.FALLING)
 
 except KeyboardInterrupt:
-	print("Exiting...")
-	GPIO.cleanup()
+    print("Exiting...")
+    GPIO.cleanup()
 
 GPIO.cleanup()
