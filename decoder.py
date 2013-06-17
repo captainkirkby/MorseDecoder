3c04d644deee5c673e6b768ad9f5eae269d3a9b1
Dylan Kirkby <dylan.kirkby@gmail.com>
2013-06-17 19:40:55 +0200

    Lint python code for cleanliness

--- decoder.py
+++ decoder.py
@@ -3,8 +3,9 @@ dit = 1
 dah = 2
 #END CONSTANTS
 
+
 class char():
-    def __init__(self,name,left=None,right=None):
+    def __init__(self, name, left=None, right=None):
         self.name = name
         self.left = left
         self.right = right
@@ -83,17 +84,18 @@ char('start',
 ))
 #END MORE CONSTANTS
 
+
 def decode(characterList):
     #the error variable marks if the character is not known by the decoding program
     error = False
     #if the length of the 'letter' is more than five dits or dahs, it can't be right so it immediatly sets error to true
-    if len(characterList)>5:
+    if len(characterList) > 5:
         error = True
     #possible is a recursively narrowing down list of possible characters
     location = tree
     for character in characterList:
         #if there is any cause for error, error is true, so it immediately returns as unknown
-        if error == True:
+        if error:
             return '?'
         if character == dit:
             location = location.right
@@ -102,8 +104,8 @@ def decode(characterList):
         else:
             #if any character lacks a 'dit or 'dot', trigger an error. Other causes for error could be more than 5 characters, or 5 characters if they do not signify a character
             error = True
-        if location == None:
+        if location is None:
             error = True
-    if error == True:
+    if error:
         return '?'
     return location.name
--- morseDecoder.py
+++ morseDecoder.py
@@ -8,18 +8,19 @@ GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 #constants
 STANDARD_BOUNCE_TIME = 60
-MIN_KEY_TIME=0.15
-MIN_DELAY_TIME=0.10
+MIN_KEY_TIME = 0.15
+MIN_DELAY_TIME = 0.10
 LEARNING_CYCLE = 8
 
 #morse constants
-DIT=1
-DAH=2
+DIT = 1
+DAH = 2
 
 #globals
 pressedDownTime = 0
 pressedUpTime = 0
-pressedDurations = [ 0.25, 0.5 ]
+pressedDurations = [0.25, 0.5]
+
 
 def buttonPressed(channel):
     """Callback called when GPIO detects button is pressed"""
@@ -30,10 +31,11 @@ def buttonPressed(channel):
     else:
         print("Button Depressed")
         global pressedDownTime
-        pressedDownTime=time.time()
+        pressedDownTime = time.time()
         GPIO.remove_event_detect(23)
         GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.add_event_detect(23, GPIO.RISING, callback=buttonReleased)
+
 
 def buttonReleased(channel):
     """Callback called when GPIO detects button is released"""
@@ -48,19 +50,18 @@ def buttonReleased(channel):
         GPIO.remove_event_detect(23)
         GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.add_event_detect(23, GPIO.FALLING, callback=buttonPressed)
-        
+        #get the character
         morseChar = detectCharacter(pressedDuration)
         print(morseToString(morseChar))
-        
         #record the last few durations
         global pressedDurations
         if len(pressedDurations) >= LEARNING_CYCLE:
             #remove last item
             del pressedDurations[(len(pressedDurations)-1)]
-        
         pressedDurations.append(pressedDuration)
         print(pressedDuration)
-    
+
+
 def detectCharacter(duration):
     """Determines if a duration represents a DIT or a DAH"""
     global pressedDurations
@@ -68,24 +69,24 @@ def detectCharacter(duration):
     pressedDurationsCopy.sort()
     #find highest (TODO:non-outlier) duration and assume it is DAH
     dahBenchmark = pressedDurationsCopy[len(pressedDurationsCopy)-1]
-    
     #calculate ideal dit as half the duration of the dah benchmark
     ditBenchmark = dahBenchmark/2
-    
     #compare duration to benchmarks
     if duration <= ((ditBenchmark+dahBenchmark)/2):
         return DIT
     else:
         return DAH
 
+
 def morseToString(morseChar):
-    if morseChar == DIT: return "DIT"
-    elif morseChar == DAH: return "DAH"
+    if morseChar == DIT:
+        return "DIT"
+    elif morseChar == DAH:
+        return "DAH"
     return "?"
 
-GPIO.add_event_detect(23, GPIO.FALLING,
-        callback=buttonPressed,
-        bouncetime=STANDARD_BOUNCE_TIME)
+
+GPIO.add_event_detect(23, GPIO.FALLING, callback=buttonPressed, bouncetime=STANDARD_BOUNCE_TIME)
 
 try:
     GPIO.wait_for_edge(24, GPIO.FALLING)
